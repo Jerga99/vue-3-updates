8a2f7e2dec9a0a2f3b6d0aa75994dea8db1665ff
Filip Jerga <jerga99@gmail.com>
2020-08-16 22:25:52 +0200

    Multiple models]

--- src/ResourceNew.vue
+++ src/ResourceNew.vue
@@ -1,7 +1,29 @@
 <template>
-  Just Some Empty Page
+  <resource-form
+    v-model:title="title"
+    v-model:description="description"
+    v-model:type="type"
+    v-model:link="link"
+    @on-form-submit="submitForm"
+  />
 </template>
 
 <script>
-  export default {}
+  import ResourceForm from '@/components/ResourceForm'
+  export default {
+    components: { ResourceForm },
+    data() {
+      return {
+        title: '',
+        description: '',
+        type: '',
+        link: ''
+      }
+    },
+    methods: {
+      submitForm() {
+        alert(`${this.title} - ${this.description} - ${this.type} - ${this.link}`)
+      }
+    }
+  }
 </script>
